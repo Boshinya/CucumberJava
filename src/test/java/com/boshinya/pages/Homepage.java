48bd89b760a1b78108971210abe0605fd540058c
Boothiraj Palanisamy <Boothiraj.Palanisamy@thomsonreuters.com>
2017-02-27 22:11:13 +0000

    Re-factored Pageobjects to pages.

--- src/test/java/com/boshinya/pages/Homepage.java
+++ src/test/java/com/boshinya/pages/Homepage.java
@@ -1,12 +1,9 @@
-package com.boshinya.pageobjects;
+package com.boshinya.pages;
 
-import com.boshinya.utilities.PropertyReader;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
--- src/test/java/com/boshinya/pages/Login.java
+++ src/test/java/com/boshinya/pages/Login.java
@@ -1,16 +1,11 @@
-package com.boshinya.pageobjects;
+package com.boshinya.pages;
 
-import com.boshinya.utilities.DriverFactory;
 import com.boshinya.utilities.PropertyReader;
-import cucumber.api.java.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.List;
 
 /**
  * Created by bothees on 11/10/2016.
